cfbb8cdcfe6ce7593c1f6416f74bc2a5556428b2
AKASH B <147173640+Akash242005@users.noreply.github.com>
2025-01-15 13:04:17 +0530

    Update index.js

--- Clock/index.js
+++ Clock/index.js
@@ -1,7 +1,9 @@
 function updateClock(){
     const now = new Date();
-    const hour = now.getHours().toString().padStart(2,0);
+    let hour = now.getHours().toString().padStart(2,0);
     const hours = hour>=12 ? "PM" : "AM";
+    hour = hour%12 || 12;
+    hour = hour.toString().padStart(2,0)
     const minintue  = now.getMinutes().toString().padStart(2,0);
     const sec = now.getSeconds().toString().padStart(2,0);
     const time = `${hour}:${minintue}:${sec} ${hours}`;
